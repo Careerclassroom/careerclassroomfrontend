12bc90f3b7fe8a48e8e7a9f6394ed7b3dee13fc7
Pradeep2888 <pradeeptiwari2888@gmail.com>
2023-08-07 00:42:45 +0530

    created contact card

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -4,29 +4,68 @@ import PageHeaer from '../components/PageHeaer'
 import Avatar from "../images/enroll-now.png"
 import Map from "../images/map.png"
 import ContactUsForm from '../components/ContactUsForm'
+import ContactUsCard from '../components/ContactUsCard'
+import Location from "../images/location.svg"
+import Timer from "../images/timer.svg"
+import Mail from "../images/mail.svg"
+import Call from "../images/phone.svg"
+
+
+
+
+const cardData=[
+  {
+    "id":1,
+    "name":"Visit Our Place",
+    "icon":Location,
+    "text":"Lorem Ipsum is simply dummy text of the printing and typesetting"
+  },
+  {
+    "id":2,
+    "name":"Office Schedule",
+    "icon":Timer,
+    "text":"Lorem Ipsum is simply dummy text of the printing and typesetting"
+  },
+  {
+    "id":3,
+    "name":"Email Address",
+    "icon":Mail,
+    "text":"Lorem Ipsum is simply dummy text of the printing and typesetting"
+  },
+  {
+    "id":4,
+    "name":"Quick Contact",
+    "icon":Call,
+    "text":"Lorem Ipsum is simply dummy text of the printing and typesetting"
+  },
+]
 
 
 function ContactUs() {
+
+
   return (
     <div>
-        <Header/>
-        <PageHeaer heading={"Contact Us"} backgroundimage={Avatar}  />
-        <section className='contactus-area' >
+      <Header />
+      <PageHeaer heading={"Contact Us"} backgroundimage={Avatar} />
+      <section className='contactus-area' >
 
         <section className='contactus-container1' >
 
-              <div className="contactus-map" >
-                   <img src={Map} alt='map' />
-              </div>
+          <div className="contactus-map" >
+            <img src={Map} alt='map' />
+          </div>
 
-               <ContactUsForm/>
+          <ContactUsForm />
         </section>
 
         <section className='contactus-container2' >
-          
+              {
+                cardData.map((index)=><ContactUsCard key={index.id} name={index.name} icon={index.icon} text={index.text} />)
+              }
         </section>
 
-        </section>
+      </section>
 
     </div>
   )
