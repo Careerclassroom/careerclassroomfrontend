a4e7161337f4138d5452e490b8014075b818d8fc
MOHAK12309 <yashraj63951@gmail.com>
2023-09-25 00:49:50 +0530

    mohak

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -25,22 +25,22 @@ function ProfilePage() {
 
   const userCookie = getCookie('user');
   const decodedUserId = decodeURIComponent(userCookie);
- 
+
 
   const idString = decodedUserId
   const regex = /"([^"]+)"/; // Regular expression to extract text within double quotes
-  
+
   const match = idString.match(regex);
- const extractedObjectId=match[1]
-  
+  const extractedObjectId = match
+
   if (match && match[1]) {
     const extractedObjectId = match[1];
     console.log(extractedObjectId); // This will print "64eb9276ebdb91fb1339b632"
   } else {
     console.log('Object ID not found or in an unexpected format.');
   }
-console.log(extractedObjectId,"hi")
- 
+  console.log(extractedObjectId, "hi")
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -52,13 +52,13 @@ console.log(extractedObjectId,"hi")
   const [showUpload, setUpload] = useState(false)
   const [bio, setBio] = useState("")
   const [show, setShow] = useState("name")
-  const [selected , setSelected] = useState([]);
+  const [selected, setSelected] = useState([]);
   const [data, setData] = useState([])
   const [profile, setProfile] = useState("profile")
   const id = useSelector((state) => state.get_seller_profile_id.user_id);
   const username = useSelector((state) => state.get_seller_profile_id.name);
-  
-  
+
+
 
   const navigate = useNavigate("")
   const handleLogout = async (e) => {
@@ -74,10 +74,10 @@ console.log(extractedObjectId,"hi")
         dispatch(getUserIdFromAuth({}));
 
         toast("Logout successfull")
-      
-          navigate("/")
-          
-       
+
+        navigate("/")
+
+
       }
     } catch (err) {
       // console.log(err);
@@ -104,7 +104,7 @@ console.log(extractedObjectId,"hi")
 
   const intrest = [
     { label: "JAVA " },
-    { label: "Python"},
+    { label: "Python" },
     { label: "DSA" },
     { label: "Data Science" },
 
@@ -128,7 +128,7 @@ console.log(extractedObjectId,"hi")
       // dispatch(getUserIdFromAuth(response.data.data.user._id, response.data.data.user.name, response.data.data.user.email));
       if (response.data.status === "success") {
         toast("Profile updated success")
-       
+
         // dispatch(getUserIdFromAuth(response.data.data.user._id,  response.data.data.user.name, response.data.data.user.email));
         console.log(response.data.data.user._id)
 
@@ -187,170 +187,170 @@ console.log(extractedObjectId,"hi")
           return (
 
             <section key={item._id} className='profile_container' >
-              { profile ==="profile" &&
-              <div className='profile_card_area' >
-              <svg style={{float:"right"}}  onClick={()=>setProfile("editprofile")} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                              <path d="M7 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V17" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                              <path d="M16 5.00011L19 8.00011M20.385 6.58511C20.7788 6.19126 21.0001 5.65709 21.0001 5.10011C21.0001 4.54312 20.7788 4.00895 20.385 3.61511C19.9912 3.22126 19.457 3 18.9 3C18.343 3 17.8088 3.22126 17.415 3.61511L9 12.0001V15.0001H12L20.385 6.58511Z" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                <div className='profile_box_1' >
-                  <div className='profile_avatar'>
-                  
+              {profile === "profile" &&
+                <div className='profile_card_area' >
+                  <svg style={{ float: "right" }} onClick={() => setProfile("editprofile")} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M7 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V17" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M16 5.00011L19 8.00011M20.385 6.58511C20.7788 6.19126 21.0001 5.65709 21.0001 5.10011C21.0001 4.54312 20.7788 4.00895 20.385 3.61511C19.9912 3.22126 19.457 3 18.9 3C18.343 3 17.8088 3.22126 17.415 3.61511L9 12.0001V15.0001H12L20.385 6.58511Z" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                  <div className='profile_box_1' >
+                    <div className='profile_avatar'>
+
                       <img src={item.photo !== undefined ? `https://classroomdata.s3.ap-south-1.amazonaws.com/${item.photo}` : item.googlePhoto} width="100" height="100" alt="" />
                       <div class="round">
 
                         <div>
-                      
-                
-                      </div>
 
 
-                    </div>
-                    {showUpload == "show" &&
-                      <button className='upload-photo-button' onClick={handleSubmitphoto} >upload</button>
-                    }
-                  </div>
-                  <h2 style={{marginTop:"30px",color:"rgba(18, 3, 127, 1)"}}>{item.name}</h2>
-
-
-                </div>
-              <div style={{textAlign:"left",marginTop:"30px",marginLeft:"20px",width:"250px",height:"auto"}}>
-                <h5 >Bio</h5>
-                <p>{item.bio}</p>
-                
-              </div>
-              <h5 style={{}}>MY Intrest</h5>
-              <div style={{textAlign:"center",marginTop:"30px",margin:"auto",width:"90%",height:"auto",display:"flex",flexWrap:"wrap"}}>
-              
-                {
-                  item.intrest.map((items)=>{
-                    return(
-                        <p style={{background:"rgba(18, 3, 127, 1)",textAlign:"center",color:"white",borderRadius:"20px",padding:"10px 30px 10px 30px",margin:"30px auto"}}>{items.label}</p>
-
-                    )
-                  
-
-                  })
-                }
-               
-              </div>
-              <div className='profile_box_4' >
-                      <button onClick={handleLogout} className='pb4_button' >LOGOUT</button>
-                    </div>
-              </div>
-        }
-          { profile==="editprofile"&&
-              <div className='profile_card_area' >
-                <div className='profile_box_1' >
-                  <div className='profile_avatar'>
-                    <div class="upload">
-                      <img src={`https://classroomdata.s3.ap-south-1.amazonaws.com/${item.photo}`} width="100" height="100" alt="" />
-                      <div class="round">
-
-                        <div>
-                          <input onChange={handleFileChange} type="file" />
-
-
-
-
-                          <i class="fa fa-camera" ></i>
                         </div>
 
 
                       </div>
-
-
+                      {showUpload == "show" &&
+                        <button className='upload-photo-button' onClick={handleSubmitphoto} >upload</button>
+                      }
                     </div>
-                    {showUpload == "show" &&
-                      <button className='upload-photo-button' onClick={handleSubmitphoto} >upload</button>
-                    }
-                  </div>
-              
+                    <h2 style={{ marginTop: "30px", color: "rgba(18, 3, 127, 1)" }}>{item.name}</h2>
 
+
+                  </div>
+                  <div style={{ textAlign: "left", marginTop: "30px", marginLeft: "20px", width: "250px", height: "auto" }}>
+                    <h5 >Bio</h5>
+                    <p>{item.bio}</p>
+
+                  </div>
+                  <h5 style={{}}>MY Intrest</h5>
+                  <div style={{ textAlign: "center", marginTop: "30px", margin: "auto", width: "90%", height: "auto", display: "flex", flexWrap: "wrap" }}>
+
+                    {
+                      item.intrest.map((items) => {
+                        return (
+                          <p style={{ background: "rgba(18, 3, 127, 1)", textAlign: "center", color: "white", borderRadius: "20px", padding: "10px 30px 10px 30px", margin: "30px auto" }}>{items.label}</p>
+
+                        )
+
+
+                      })
+                    }
+
+                  </div>
+                  <div className='profile_box_4' >
+                    <button onClick={handleLogout} className='pb4_button' >LOGOUT</button>
+                  </div>
                 </div>
-                <form onSubmit={handleSignUp}>
-                  <div>
+              }
+              {profile === "editprofile" &&
+                <div className='profile_card_area' >
+                  <div className='profile_box_1' >
+                    <div className='profile_avatar'>
+                      <div class="upload">
+                        <img src={`https://classroomdata.s3.ap-south-1.amazonaws.com/${item.photo}`} width="100" height="100" alt="" />
+                        <div class="round">
+
+                          <div>
+                            <input onChange={handleFileChange} type="file" />
+
+
+
+
+                            <i class="fa fa-camera" ></i>
+                          </div>
+
+
+                        </div>
+
+
+                      </div>
+                      {showUpload == "show" &&
+                        <button className='upload-photo-button' onClick={handleSubmitphoto} >upload</button>
+                      }
+                    </div>
+
+
+                  </div>
+                  <form onSubmit={handleSignUp}>
                     <div>
+                      <div>
 
-                      <div className='profile_avatar_content'>
-                        <div className='pac_name'>
-                          <div>
-                          
-                              
-
+                        <div className='profile_avatar_content'>
+                          <div className='pac_name'>
+                            <div>
 
 
-                            {show == "edit" &&
-                              <div style={{ display: "flex" }}>
-                                <input type='text' className='name-input' placeholder='name' value={name} onChange={(e) => setName(e.target.value)} ></input>
-                                <div onClick={(e) => !setShow("name")} style={{ cursor: "pointer", marginLeft: "20px" }}>X</div>
-                              </div>
-                            }
 
+
+
+                              {show == "edit" &&
+                                <div style={{ display: "flex" }}>
+                                  <input type='text' className='name-input' placeholder='name' value={name} onChange={(e) => setName(e.target.value)} ></input>
+                                  <div onClick={(e) => !setShow("name")} style={{ cursor: "pointer", marginLeft: "20px" }}>X</div>
+                                </div>
+                              }
+
+                            </div>
+                            <div>
+
+
+                            </div>
                           </div>
-                          <div>
 
-                         
+                        </div>
+                      </div>
+                      <div className='profile_box_2'  >
+                        <div className='pb2c1'>
+                          <div className='pacn_bio'><p>Name</p></div>
+                          <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                              <path d="M7 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V17" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                              <path d="M16 5.00011L19 8.00011M20.385 6.58511C20.7788 6.19126 21.0001 5.65709 21.0001 5.10011C21.0001 4.54312 20.7788 4.00895 20.385 3.61511C19.9912 3.22126 19.457 3 18.9 3C18.343 3 17.8088 3.22126 17.415 3.61511L9 12.0001V15.0001H12L20.385 6.58511Z" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
                           </div>
                         </div>
-                   
-                      </div>
-                    </div>
-                    <div className='profile_box_2'  >
-                      <div className='pb2c1'>
-                        <div className='pacn_bio'><p>Name</p></div>
-                        <div>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <path d="M7 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V17" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M16 5.00011L19 8.00011M20.385 6.58511C20.7788 6.19126 21.0001 5.65709 21.0001 5.10011C21.0001 4.54312 20.7788 4.00895 20.385 3.61511C19.9912 3.22126 19.457 3 18.9 3C18.343 3 17.8088 3.22126 17.415 3.61511L9 12.0001V15.0001H12L20.385 6.58511Z" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                          </svg>
+                        <div className='pb2c2'>
+                          <input value={name} onChange={(e) => setName(e.target.value)} />
                         </div>
-                      </div>
-                      <div className='pb2c2'>
-                        <input value={name} onChange={(e) => setName(e.target.value)} />
-                      </div>
 
-                    </div>
-                    <div className='profile_box_2' >
-                      <div className='pb2c1'>
-                        <div className='pacn_bio'><p>Bio</p></div>
-                        <div>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <path d="M7 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V17" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M16 5.00011L19 8.00011M20.385 6.58511C20.7788 6.19126 21.0001 5.65709 21.0001 5.10011C21.0001 4.54312 20.7788 4.00895 20.385 3.61511C19.9912 3.22126 19.457 3 18.9 3C18.343 3 17.8088 3.22126 17.415 3.61511L9 12.0001V15.0001H12L20.385 6.58511Z" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                          </svg>
+                      </div>
+                      <div className='profile_box_2' >
+                        <div className='pb2c1'>
+                          <div className='pacn_bio'><p>Bio</p></div>
+                          <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                              <path d="M7 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V17" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                              <path d="M16 5.00011L19 8.00011M20.385 6.58511C20.7788 6.19126 21.0001 5.65709 21.0001 5.10011C21.0001 4.54312 20.7788 4.00895 20.385 3.61511C19.9912 3.22126 19.457 3 18.9 3C18.343 3 17.8088 3.22126 17.415 3.61511L9 12.0001V15.0001H12L20.385 6.58511Z" stroke="#12037F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                          </div>
                         </div>
-                      </div>
-                      <div className='pb2c2'>
-                        <input value={bio} onChange={(e) => setBio(e.target.value)} />
-                      </div>
-
-                    </div>
-                    <div className='profile_box_3' >
-
-                      <div className='pb2c1'>
-                        <div className='pacn_bio'><p>Interest</p></div>
+                        <div className='pb2c2'>
+                          <input value={bio} onChange={(e) => setBio(e.target.value)} />
+                        </div>
 
                       </div>
-                      <Multiselect options={intrest} required selectedValues={selected} onSelect={(selectedList) => setSelected(selectedList)}
-                                    onRemove={(selectedList) => setSelected(selectedList)}
-                                    displayValue="label" placeholder="Select Intrest" />
+                      <div className='profile_box_3' >
+
+                        <div className='pb2c1'>
+                          <div className='pacn_bio'><p>Interest</p></div>
+
+                        </div>
+                        <Multiselect options={intrest} required selectedValues={selected} onSelect={(selectedList) => setSelected(selectedList)}
+                          onRemove={(selectedList) => setSelected(selectedList)}
+                          displayValue="label" placeholder="Select Intrest" />
+
+
+                      </div>
+                      <div className='profile_box_4' >
+                        <button type='submit' className='pb4_button' >SUBMIT</button>
+                      </div>
 
 
                     </div>
-                    <div className='profile_box_4' >
-                      <button type='submit' className='pb4_button' >SUBMIT</button>
-                    </div>
-                 
-                   
-                  </div>
                   </form>
                   <div className='profile_box_4' >
-                      <button onClick={()=>setProfile("profile")} className='pb4_button' >Back</button>
-                    </div>
+                    <button onClick={() => setProfile("profile")} className='pb4_button' >Back</button>
+                  </div>
 
-              </div>
-        }
+                </div>
+              }
 
             </section>
 
