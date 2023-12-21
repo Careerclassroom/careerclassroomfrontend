6a64c49fa15cdc034cd9ec18ed67e3c129067773
MOHAK12309 <yashraj63951@gmail.com>
2023-12-21 18:41:25 +0530

    m

--- src/components/Enrollment.js
+++ src/components/Enrollment.js
@@ -75,7 +75,7 @@ function Enroll() {
       
     try {
        
-        const response = await axios.post('https://server.careerclassroom.in/api/v1/order/payment/64eb22ad3330b861fc6b1e20', {
+        const response = await axios.post('https://server.careerclassroom.in/api/v1/order/payment/64eb8c0a0f8404f8f066d7f9', {
             MobileNumber: number,
             amount: 1,
             redirectUrl: "https://careerclassroom.in/home",
