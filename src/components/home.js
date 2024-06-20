7e761c886bda5ecd44177e1513b1d2bf6414d112
MOHAK12309 <yashraj63951@gmail.com>
2024-06-20 11:34:18 +0530

    m

--- src/components/home.js
+++ src/components/home.js
@@ -10,7 +10,8 @@ import { useSelector } from "react-redux";
 import { getPay } from "../Redux/actions/getPayid";
 import { useDispatch } from "react-redux";
 import toast from "react-hot-toast";
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 const no_profile = new URL("../images/arrow.png", import.meta.url);
 const About = new URL("../images/about.png", import.meta.url);
 const powerBi = new URL("../images/powerBi.png", import.meta.url);
@@ -30,17 +31,35 @@ const id2 = document.cookie;
 console.log("id", decodeURIComponent(id2));
 
 function Home() {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 600, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+
   const [arrow, setArrow] = useState(true);
   const baseUrl = "https://server.careerclassroom.in";
   const baseUrls = "http://localhost:8000";
   const dispatch = useDispatch("");
   const id = useSelector((state) => state.get_seller_profile_id.user_id);
   const id2 = useSelector((state) => state.get_pay_id.trans_id);
-  const cname=useSelector((state) => state.get_pay_id.course_name);
-  const cid=useSelector((state) => state.get_pay_id.course_id);
- const cdes=useSelector((state) => state.get_pay_id.course_description);
- const cpic=useSelector((state) => state.get_pay_id.course_photo);
- 
+  const cname = useSelector((state) => state.get_pay_id.course_name);
+  const cid = useSelector((state) => state.get_pay_id.course_id);
+  const cdes = useSelector((state) => state.get_pay_id.course_description);
+  const cpic = useSelector((state) => state.get_pay_id.course_photo);
+  const [data, setData] = useState([]);
   const userEmail = useSelector((state) => state.get_seller_profile_id.email);
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -52,7 +71,7 @@ function Home() {
           `${baseUrl}/api/v1/order/sendPaymentSucces`,
           {
             email: userEmail,
-            order:id2
+            order: id2,
           }
         );
         console.log(response);
@@ -71,11 +90,11 @@ function Home() {
         const response = await axios.patch(
           `${baseUrl}/api/v1/order/updateData/${id}`,
           {
-            courseId:cid,
-            courseDescription:cdes,
-        
-            courseName:cname,
-            coursePhoto:cpic
+            courseId: cid,
+            courseDescription: cdes,
+
+            courseName: cname,
+            coursePhoto: cpic,
           }
         );
         console.log(response);
@@ -86,6 +105,18 @@ function Home() {
       } catch (error) {
         console.log(error);
       }
+    }
+  };
+  useEffect(() => {
+    handleGetOneUser();
+  }, []);
+  const handleGetOneUser = async () => {
+    try {
+      const res = await axios.get(`${baseUrl}/api/v1/admin/getAllCourses`);
+      setData(res.data.data.AllCourse);
+      console.log(res)
+    } catch (err) {
+      console.log(err);
     }
   };
   useEffect(() => {
@@ -119,46 +150,105 @@ function Home() {
     <>
       <Header />
       <div style={{ marginTop: "100px" }} className="home-head">
-        <div className="home-head-child">
-          <h1
-            style={{
-              marginBottom: "50px",
+        <Carousel
+          minimumTouchDrag={80}
+          swipeable={true}
+          draggable={true}
+          showDots={false}
+          responsive={responsive}
+          arrows={false}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlaySpeed={4000}
+          autoPlay={true}
+          keyBoardControl={true}
+          removeArrowOnDeviceType={["tablet", "mobile","computer"]}
+          transitionDuration={500}
+          containerClass="rideSlide"
+        >
+          {data.map((item) => {
+            return (
+              <div className="home-head-child">
+                <h1
+                  style={{
+                    marginBottom: "50px",
 
-              color: "#12037F",
-              fontWeight: "bolder",
-            }}
-          >
-            Exciting News !
-          </h1>
-          <h1
-            style={{
-              marginBottom: "40px",
-              color: "#12037F",
-              fontWeight: "bold",
-            }}
-          >
-            Unleash the Power of BI with Industry Experts
-          </h1>
-          <div className="join">
-            <h2 className="join-content">
-              Join us for an exclusive LIVE workshop on Power BI, starting on
-              10th August!
-            </h2>
-          </div>
-          <button
-            onMouseOver={() => setArrow(false)}
-            onMouseLeave={() => setArrow(true)}
-            style={{ marginTop: "50px" }}
-            className="head-btn"
-          >
-            <Link
-              style={{ textDecoration: "none", color: "white" }}
-              to="/enroll"
+                    color: "#12037F",
+                    fontWeight: "bolder",
+                  }}
+                >
+                  Exciting News !
+                </h1>
+                <h1
+                  style={{
+                    marginBottom: "40px",
+                    color: "#12037F",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {item.CourseName}
+                </h1>
+                <div className="join">
+                  <h2 className="join-content">{item.Description.slice(0,200)}...</h2>
+                </div>
+                <button
+                  onMouseOver={() => setArrow(false)}
+                  onMouseLeave={() => setArrow(true)}
+                  style={{ marginTop: "50px" }}
+                  className="head-btn"
+                >
+                  <Link
+                    style={{ textDecoration: "none", color: "white" }}
+                    to={`/explore-course/${item._id}`}
+                  >
+                    Enroll Now{" "}
+                  </Link>
+                </button>
+              </div>
+            );
+          })}
+          {/* <div className="home-head-child">
+            <h1
+              style={{
+                marginBottom: "50px",
+
+                color: "#12037F",
+                fontWeight: "bolder",
+              }}
             >
-              Enroll Now{" "}
-            </Link>
-          </button>
-        </div>
+              Exciting News !
+            </h1>
+            <h1
+              style={{
+                marginBottom: "40px",
+                color: "#12037F",
+                fontWeight: "bold",
+              }}
+            >
+              Unleash the Power of BI with Industry Experts
+            </h1>
+            <div className="join">
+              <h2 className="join-content">
+                Join us for an exclusive LIVE workshop on Power BI, starting on
+                10th August!
+              </h2>
+            </div>
+            <button
+              onMouseOver={() => setArrow(false)}
+              onMouseLeave={() => setArrow(true)}
+              style={{ marginTop: "50px" }}
+              className="head-btn"
+            >
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/enroll"
+              >
+                Enroll Noxw{" "}
+              </Link>
+            </button>
+          </div> */}
+        </Carousel>
+      
       </div>
       <div>
         <h2
@@ -173,10 +263,7 @@ function Home() {
           </div>
           <div className="about-bi-flex2">
             <h3 style={{ marginTop: "50px" }}>Welcome to Career classroom?</h3>
-            <p
-              
-              className="home-description"
-            >
+            <p className="home-description">
               At Careerclassroom, we believe that everyone deserves the
               opportunity to thrive in their careers and achieve their
               professional goals. Our mission is to empower individuals with the
@@ -199,30 +286,91 @@ function Home() {
         >
           <u>Course</u>
         </h2>
-        <div className="course">
-          <div className="course1">
-            <p style={{}} className="para-font">
-              4 Days Bootcamp on Power BI Expert &<br></br> Excel with Project.
-              [ 1 hour per day ]
-            </p>
-            <button className="head-btn">
+        <Carousel
+          minimumTouchDrag={80}
+          swipeable={true}
+          draggable={true}
+          showDots={false}
+          responsive={responsive}
+          arrows={false}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlaySpeed={4000}
+          autoPlay={true}
+          keyBoardControl={true}
+          removeArrowOnDeviceType={["tablet", "mobile","computer"]}
+          transitionDuration={500}
+          containerClass="rideSlide"
+        >
+          {data.map((item) => {
+            return (
+        
+              <div className="course">
+              <div className="course1">
+                <p style={{}} className="para-font">
+                  {item.CourseName}
+                </p>
+                <button className="head-btn">
+                  <Link
+                    style={{ textDecoration: "none", color: "white" }}
+                    to={`/explore-course/${item._id}`}
+                  >
+                    Enroll Now{" "}
+                  </Link>{" "}
+                </button>
+    
+                <p style={{ marginTop: "20px" }} className="para-font2">
+                  Charges for 4 Days Boot Camp are 499 / student
+                </p>
+              </div>
+              <div className="course2">
+                <img width="100%" height="330px" src={`https://classroomdata.s3.ap-south-1.amazonaws.com/${item.CoursePhoto}`}></img>
+              </div>
+            </div>
+            );
+          })}
+          {/* <div className="home-head-child">
+            <h1
+              style={{
+                marginBottom: "50px",
+
+                color: "#12037F",
+                fontWeight: "bolder",
+              }}
+            >
+              Exciting News !
+            </h1>
+            <h1
+              style={{
+                marginBottom: "40px",
+                color: "#12037F",
+                fontWeight: "bold",
+              }}
+            >
+              Unleash the Power of BI with Industry Experts
+            </h1>
+            <div className="join">
+              <h2 className="join-content">
+                Join us for an exclusive LIVE workshop on Power BI, starting on
+                10th August!
+              </h2>
+            </div>
+            <button
+              onMouseOver={() => setArrow(false)}
+              onMouseLeave={() => setArrow(true)}
+              style={{ marginTop: "50px" }}
+              className="head-btn"
+            >
               <Link
                 style={{ textDecoration: "none", color: "white" }}
                 to="/enroll"
               >
-                Enroll Now{" "}
-              </Link>{" "}
-            
+                Enroll Noxw{" "}
+              </Link>
             </button>
-
-            <p style={{ marginTop: "20px" }} className="para-font2">
-              Charges for 4 Days Boot Camp are 499 / student
-            </p>
-          </div>
-          <div className="course2">
-            <img width="100%" height="330px" src={powerBi}></img>
-          </div>
-        </div>
+          </div> */}
+        </Carousel>
+      
       </div>
       <Footer />
     </>
